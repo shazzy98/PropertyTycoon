991a5d447b436e1de64a7fb5fe17db0fad39ba16
shazzy98 <56957550+shazzy98@users.noreply.github.com>
2020-03-07 12:23:57 +0000

    Update2

---
+++ Game/Assets/Property Tycoon/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DiceManager : MonoBehaviour 
+{
+    public List<Dice> diceList;
+    public int totalValue;
+    public UnityEvent EndRollEvent;
+
+    public void CountAllDieValues()
+    {
+        for (int i = 0; i < diceList.Count; i++)
+        {
+            if(diceList[i].isRolling == true)
+            {
+                totalValue = 0;
+                return;
+            }
+            else
+            {
+                totalValue += diceList[i].value;
+            }
+        }
+        EndRollEvent.Invoke();
+    }
+
+    public void RollAllDie()
+    {
+        totalValue = 0;
+
+        for (int i = 0; i < diceList.Count; i++)
+        {
+            diceList[i].AddForceToDice();
+        }
+    }
+
+
+
+
+
+
+
+
+}
