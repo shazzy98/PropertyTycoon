5c45a72ded9e8d2dfa5a2a7dc5fae35b3c4ad189
shazzy98 <56957550+shazzy98@users.noreply.github.com>
2020-03-09 17:48:44 +0000

    Added Balance

---
+++ Game/Assets/Property Tycoon/Assets/Scripts/money.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class money : MonoBehaviour
+{
+    public int balance;
+    public Text balanceText;
+    
+
+    void Start()
+    {
+        balance = 1500;
+        balanceText.text = balance.ToString();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void addMoney(int addedMoney)
+    {
+        balance += addedMoney;
+        balanceText.text = balance.ToString();
+    }
+
+    public void subtractMoney (int subtractedMoney)
+    {
+        
+            balance -= subtractedMoney;
+            balanceText.text = balance.ToString();
+
+    } 
+
+}
