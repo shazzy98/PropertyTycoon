d439b11f92f0244d53fa0f675162551233ac0698
JedWalton <jedwalton98@gmail.com>
2020-05-08 16:40:56 +0100

    Stations rent charging and buy property
    Stations rent charging and buy property functionality added.

--- Game/Assets/Property Tycoon/Assets/Scripts/Trigger Event Scripts For Player/TriggerEvents/TriggerEventHoveStation16.cs
+++ Game/Assets/Property Tycoon/Assets/Scripts/Trigger Event Scripts For Player/TriggerEvents/TriggerEventHoveStation16.cs
@@ -12,58 +12,279 @@ public class TriggerEventHoveStation16 : MonoBehaviour
     string Action = "";
     string canBeBought = "Yes";
     string OwnedBy = "";
+    string TypeOfPropertyOwned = "";
     int Cost = 200;
     int unimprovedRent = 0;
-    int PropertyOne = 25;
-    int PropertyTwo = 50;
-    int PropertyThree = 100;
-    int PropertyFour = 200;
+    int oneStation = 25;
+    int twoStation = 50;
+    int threeStation = 100;
+    int fourStation = 200;
     int hotel = 0;
-
     float timer = 0;
 
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag ("Player"))
         {
+            timer = timer + Time.deltaTime;
             if ((timer > 1) && timer <1.1)
             {
-                Debug.Log("Player Moved to Hove Station");
+                Debug.Log("Player Moved to Brighton Station");
+                if (OwnedBy == null)
+                {
+                    Debug.Log("would player like to buy property");//waiting on user interface
+                    int yes = 1;
+                    if (yes == 1)
+                    {
+                        GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().subtractMoney(200);
+                        OwnedBy = "Boot (Tag 'Player')";
+                        GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().purchasedStation();
+                        int noOfStations = GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().StationsOwnedBy();
+                    }
+                }
+                if (OwnedBy != null)
+                {
+                    int noOfStations = GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().StationsOwnedBy();
+                    Debug.Log("you have been charged with rent");
+                    switch (noOfStations)
+                    {
+                        case 0:
+                            GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().subtractMoney(0);
+                            break;
+                        case 1:
+                            GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().subtractMoney(25);
+                            break;
+                        case 2:
+                            GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().subtractMoney(50);
+                            break;
+                        case 3:
+                            GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().subtractMoney(100);
+                            break;
+                        case 4:
+                            GameObject.Find ("Boot (Tag 'Player')").GetComponent<money>().subtractMoney(200);
+                            break;
+
+                    }
+                }
             }
         }
         if (other.CompareTag ("Player2"))
         {
+            timer = timer + Time.deltaTime;
             if ((timer > 1) && timer <1.1)
             {
-                Debug.Log("Player2 Moved to Hove Station");
+                Debug.Log("Player2 Moved to Brighton Station");
+                if (OwnedBy == null)
+                {
+                    Debug.Log("would player like to buy property");//waiting on user interface
+                    int yes = 1;
+                    if (yes == 1)
+                    {
+                        GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().subtractMoney(200);
+                        OwnedBy = "Smartphone (Tag 'Player2')";
+                        GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().purchasedStation();
+                        int noOfStations = GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().StationsOwnedBy();
+                    }
+                }
+                if (OwnedBy != null)
+                {
+                    int noOfStations = GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().StationsOwnedBy();
+                    Debug.Log("you have been charged with rent");
+                    switch (noOfStations)
+                    {
+                        case 0:
+                            GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().subtractMoney(0);
+                            break;
+                        case 1:
+                            GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().subtractMoney(25);
+                            break;
+                        case 2:
+                            GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().subtractMoney(50);
+                            break;
+                        case 3:
+                            GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().subtractMoney(100);
+                            break;
+                        case 4:
+                            GameObject.Find ("Smartphone (Tag 'Player2')").GetComponent<money2>().subtractMoney(200);
+                            break;
+
+                    }
+                }
             }
         }
         if (other.CompareTag ("Player3"))
         {
+            timer = timer + Time.deltaTime;
             if ((timer > 1) && timer <1.1)
             {
-                Debug.Log("Player3 Moved to Hove Station");
+                Debug.Log("Player3 Moved to Brighton Station");
+                if (OwnedBy == null)
+                {
+                    Debug.Log("would player like to buy property");//waiting on user interface
+                    int yes = 1;
+                    if (yes == 1)
+                    {
+                        GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().subtractMoney(200);
+                        OwnedBy = "Goblet (Tag 'Player3)";
+                        GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().purchasedStation();
+                        int noOfStations = GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().StationsOwnedBy();
+                    }
+                }
+                if (OwnedBy != null)
+                {
+                    int noOfStations = GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().StationsOwnedBy();
+                    Debug.Log("you have been charged with rent");
+                    switch (noOfStations)
+                    {
+                        case 0:
+                            GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().subtractMoney(0);
+                            break;
+                        case 1:
+                            GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().subtractMoney(25);
+                            break;
+                        case 2:
+                            GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().subtractMoney(50);
+                            break;
+                        case 3:
+                            GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().subtractMoney(100);
+                            break;
+                        case 4:
+                            GameObject.Find ("Goblet (Tag 'Player3)").GetComponent<money2>().subtractMoney(200);
+                            break;
+                    }
+                }
             }
         }
         if (other.CompareTag ("Player4"))
         {
+            timer = timer + Time.deltaTime;
             if ((timer > 1) && timer <1.1)
             {
-                Debug.Log("Player4 Moved to Hove Station");
+                Debug.Log("Player3 Moved to Brighton Station");
+                if (OwnedBy == null)
+                {
+                    Debug.Log("would player like to buy property");//waiting on user interface
+                    int yes = 1;
+                    if (yes == 1)
+                    {
+                        GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().subtractMoney(200);
+                        OwnedBy = "Hatstand (Tag 'Player4')";
+                        GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().purchasedStation();
+                        int noOfStations = GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().StationsOwnedBy();
+                    }
+                }
+                if (OwnedBy != null)
+                {
+                    int noOfStations = GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().StationsOwnedBy();
+                    Debug.Log("you have been charged with rent");
+                    switch (noOfStations)
+                    {
+                        case 0:
+                            GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().subtractMoney(0);
+                            break;
+                        case 1:
+                            GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().subtractMoney(25);
+                            break;
+                        case 2:
+                            GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().subtractMoney(50);
+                            break;
+                        case 3:
+                            GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().subtractMoney(100);
+                            break;
+                        case 4:
+                            GameObject.Find ("Hatstand (Tag 'Player4')").GetComponent<money4>().subtractMoney(200);
+                            break;
+
+                    }
+                }
             }
         }
         if (other.CompareTag ("Player5"))
         {
+            timer = timer + Time.deltaTime;
             if ((timer > 1) && timer <1.1)
             {
-                Debug.Log("Player5 Moved to Hove Station");
+                Debug.Log("Player3 Moved to Brighton Station");
+                if (OwnedBy == null)
+                {
+                    Debug.Log("would player like to buy property");//waiting on user interface
+                    int yes = 1;
+                    if (yes == 1)
+                    {
+                        GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().subtractMoney(200);
+                        OwnedBy = "Cat (Tag 'Player5')";
+                        GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().purchasedStation();
+                        int noOfStations = GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().StationsOwnedBy();
+                    }
+                }
+                if (OwnedBy != null)
+                {
+                    int noOfStations = GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().StationsOwnedBy();
+                    Debug.Log("you have been charged with rent");
+                    switch (noOfStations)
+                    {
+                        case 0:
+                            GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().subtractMoney(0);
+                            break;
+                        case 1:
+                            GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().subtractMoney(25);
+                            break;
+                        case 2:
+                            GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().subtractMoney(50);
+                            break;
+                        case 3:
+                            GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().subtractMoney(100);
+                            break;
+                        case 4:
+                            GameObject.Find ("Cat (Tag 'Player5')").GetComponent<money4>().subtractMoney(200);
+                            break;
+
+                    }
+                }
             }
         }
         if (other.CompareTag ("Player6"))
         {
+            timer = timer + Time.deltaTime;
             if ((timer > 1) && timer <1.1)
             {
-                Debug.Log("Player6 Moved to Hove Station");
+                Debug.Log("Player3 Moved to Brighton Station");
+                if (OwnedBy == null)
+                {
+                    Debug.Log("would player like to buy property");//waiting on user interface
+                    int yes = 1;
+                    if (yes == 1)
+                    {
+                        GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().subtractMoney(200);
+                        OwnedBy = "Spoon (Tag 'Player6')";
+                        GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().purchasedStation();
+                        int noOfStations = GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().StationsOwnedBy();
+                    }
+                }
+                if (OwnedBy != null)
+                {
+                    int noOfStations = GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().StationsOwnedBy();
+                    Debug.Log("you have been charged with rent");
+                    switch (noOfStations)
+                    {
+                        case 0:
+                            GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().subtractMoney(0);
+                            break;
+                        case 1:
+                            GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().subtractMoney(25);
+                            break;
+                        case 2:
+                            GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().subtractMoney(50);
+                            break;
+                        case 3:
+                            GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().subtractMoney(100);
+                            break;
+                        case 4:
+                            GameObject.Find ("Spoon (Tag 'Player6')").GetComponent<money6>().subtractMoney(200);
+                            break;
+
+                    }
+                }
             }
         }   
     }
---
+++ Game/Assets/Property Tycoon/Assets/Scripts/money5.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class money5 : MonoBehaviour
+{
+    public int balance;
+    public Text balanceText;
+    float timer = 0;
+    public GameObject WarpExit;
+    public bool stay = false;
+    public int StationsOwned = 0;
+
+  
+    
+    
+
+
+    void Start()
+    {
+  
+        balance = 1500;
+        balanceText.text = balance.ToString();
+        
+    }
+
+
+    void Update()
+    {
+        
+
+    }
+    public void purchasedStation()
+    {
+        StationsOwned += 1;
+    }
+    public int StationsOwnedBy()
+    {
+        return StationsOwned;
+    }
+
+    public void addMoney(int addedMoney)
+    {
+        balance += addedMoney;
+        balanceText.text = balance.ToString();
+    }
+
+    public void subtractMoney (int subtractedMoney)
+    {
+        
+            balance -= subtractedMoney;
+            balanceText.text = balance.ToString();
+
+    }
+
+    public void buyProperty()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            
+            addMoney(200);
+            
+        }
+
+    }
+
+
+
+    void OnTriggerStay(Collider other)
+    {
+       
+        if (other.gameObject.CompareTag("tax"))
+        {
+            timer = timer + Time.deltaTime;
+            if ((timer > 1) && timer <1.1)
+            {
+                stay = true;
+                tax();
+                
+            }
+
+           
+        }
+
+        if (other.gameObject.CompareTag("jail"))
+        {
+            timer = timer + Time.deltaTime;
+            if (timer > 1) 
+            { 
+            Debug.Log("Going to Jail");
+            transform.position = WarpExit.transform.position;
+            }
+        }
+
+
+    }
+
+
+
+    void tax()
+    {
+        if (stay == true)
+        {
+            Debug.Log("Income Tax 200");
+            subtractMoney(40);
+            stay = false;
+        }
+    }
+
+
+
+    }
+
+
+
+
+
+
